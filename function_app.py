befd2b1b60965aa9de56124d5939f8524be2d176
Ben Constable <benconstable@microsoft.com>
2024-07-04 16:53:52 +0100

    Update code

--- function_app.py
+++ function_app.py
@@ -3,38 +3,71 @@ import logging
 import json
 import asyncio
 from openai import AsyncAzureOpenAI
+import openai
 import os
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-async def vectorise_record(record: dict) -> dict:
-    """Vectorise the text in the record using the Azure OpenAI API."""
+async def vectorise_record(record: dict, tries_left=3) -> dict:
+    """Vectorise the text in the record using the Azure OpenAI API.
+    
+    Args:
+        record (dict): The record to vectorise.
+        
+    Returns:
+        dict: The vectorised record."""
+    
+    compass_api_key = os.environ["COMPASS_API_KEY"]
+    compass_endpoint = os.environ["COMPASS_ENDPOINT"]
+    compass_api_version = os.environ["COMPASS_API_VERSION"]
+    compass_embedding_model = os.environ["COMPASS_EMBEDDING_MODEL"]
+    
+    try:
+        async with AsyncAzureOpenAI(
+            api_key=compass_api_key,
+            azure_endpoint=compass_endpoint,
+            api_version=compass_api_version,
+        ) as open_ai_client:
+            embeddings = await open_ai_client.embeddings.create(
+                model=compass_embedding_model, input=list(record["data"].values())
+            )
 
-    api_key = os.environ["API_KEY"]
-    async with AsyncAzureOpenAI(
-        # This is the default and can be omitted
-        api_key=api_key,
-        azure_endpoint="https://api.core42.ai/",
-        api_version="2023-03-15-preview",
-    ) as open_ai_client:
+            logging.debug("Embeddings: %s", embeddings)
 
-        embeddings = await open_ai_client.embeddings.create(
-            model="text-embedding-ada-002", input=list(record["data"].values())
-        )
+    except openai.RateLimitError as e:
+        logging.error("OpenAI Rate Limit Error: %s", e)
+         
+        if tries_left > 0:
+            logging.info("Retrying vectorisation of record with %s tries left.", tries_left)
+            remaining_tries = tries_left - 1
+            backoff = 15 ** (3 - remaining_tries)
+            await asyncio.sleep(backoff)
+            return await vectorise_record(record, tries_left=remaining_tries)
+        else:
+            logging.info("Failed to vectorise record after 3 retries.")
+            return {"recordId": record["recordId"], "data": {}, "errors": [{"message": "Failed to vectorise input records with CompassAPI. Check function app logs for more details of exact failure."}], "warnings": None}
+        
+    except (openai.OpenAIError, openai.APIConnectionError) as e:
+        logging.error("OpenAI Error: %s", e)
+        return {"recordId": record["recordId"], "data": {}, "errors": [{"message": "Failed to vectorise input records with CompassAPI. Check function app logs for more details of exact failure."}], "warnings": None}
+    else:
+        vectorised_record = {"recordId": record["recordId"], "data": {}, "errors": None, "warnings": None}
+        for index, key in enumerate(record["data"].keys()):
+            vectorised_record["data"][f"{key}_vector"] = embeddings.data[index].embedding
 
-        logging.debug(f"Embeddings: {embeddings}")
+        return vectorised_record
 
-    vectorised_record = {"recordId": record["recordId"], "data": {}, "errors": None, "warnings": None}
-    for index, key in enumerate(record["data"]):
-
-        vectorised_record["data"][f"{key}_vector"] = embeddings.data[index].embedding
-
-    return vectorised_record
-
+@app.function_name(name="ai_search_2_compass")
 @app.route(route="ai_search_2_compass", methods=[func.HttpMethod.POST])
 async def ai_search_2_compass(req: func.HttpRequest) -> func.HttpResponse:
-    """HTTP trigger for AI Search 2 Compass function."""
+    """HTTP trigger for AI Search 2 Compass function.
+    
+    Args:
+        req (func.HttpRequest): The HTTP request object.
+        
+    Returns:
+        func.HttpResponse: The HTTP response object."""
 
     logging.info("Python HTTP trigger function processed a request.")
 
@@ -46,7 +79,7 @@ async def ai_search_2_compass(req: func.HttpRequest) -> func.HttpResponse:
             "Please valid Custom Skill Payload in the request body", status_code=400
         )
     else:
-        logging.debug(f"Input Values: {values}")
+        logging.debug("Input Values: %s", values)
 
         record_tasks = []
 
@@ -56,7 +89,7 @@ async def ai_search_2_compass(req: func.HttpRequest) -> func.HttpResponse:
             )
 
         results = await asyncio.gather(*record_tasks)
-        logging.debug(f"Results: {results}")
+        logging.debug("Results: %s", results)
         vectorised_tasks = {"values": results}
 
         return func.HttpResponse(json.dumps(vectorised_tasks), status_code=200, mimetype="application/json")
